c883cce3f94d8185017b47c947664ea8b3084740
MisterTB <94872162+MisterTB@users.noreply.github.com>
2023-10-03 21:49:47 -0400

    #28 Enabled Static Website Hosting, uploaded index and error file via TF (#29)

--- modules/terrahouse_aws/main.tf
+++ modules/terrahouse_aws/main.tf
@@ -12,11 +12,40 @@ terraform {
 #  # Configuration options
 #}
 
-  resource "aws_s3_bucket" "website_bucket" {
-  bucket = var.bucket_name
+resource "aws_s3_bucket" "website_bucket" {
+   bucket = var.bucket_name
 
   tags = {
     UserUuid        = var.user_uuid
     Environment     = "Dev"
   }
+}
+
+resource "aws_s3_bucket_website_configuration" "website_configuration" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_object" "index_html" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key    = "index.html"
+  #source = "${path.root}/public/index.html"
+  source = var.index_html_filepath
+
+  etag = filemd5(var.index_html_filepath)
+}
+
+resource "aws_s3_object" "error_html" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key    = "error.html"
+  source = var.error_html_filepath
+
+  etag  = filemd5(var.error_html_filepath)
 }
--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,9 @@ output "bucket_name" {
   description = "Bucket Name for SWA Hosting"
   value = module.terrahouse_aws.bucket_name
 }
+
+output "s3_website_endpoint" {
+    description = "S3 static website hosting endpoint"
+    value = module.terrahouse_aws.website_endpoint
+  
+}
