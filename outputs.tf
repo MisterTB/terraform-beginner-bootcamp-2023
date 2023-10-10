5e4510b5bcb07b303931431c4b1aca4ee387e019
MisterTB <thadbrownsc@gmail.com>
2023-10-10 03:08:49 +0000

    #34 Invalidate CF stack with content version changes.

--- outputs.tf
+++ outputs.tf
@@ -6,5 +6,9 @@ output "bucket_name" {
 output "s3_website_endpoint" {
     description = "S3 static website hosting endpoint"
     value = module.terrahouse_aws.website_endpoint
-  
+}
+
+output "cloudfront_url" {
+  description = "The CloudFront Distribution Domain"
+  value = module.terrahouse_aws.cloudfront_url
 }
